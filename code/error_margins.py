f1a26f95fff3c9a1cb1e7b1018d95632622a1d91
JonasBoselie <boseliejonas@gmail.com>
2026-06-18 11:29:35 +0200

    Update error_margins.py

--- code/error_margins.py
+++ code/error_margins.py
@@ -3,7 +3,7 @@ import os
 
 
 # path to the map with .txt files:
-path = r"./Results 03-06-2026"
+path = r"./Results 08-06-2026"
 
 # name under which the output file will be saved:
 save_name = "errors"
@@ -47,10 +47,17 @@ save_path = f"{path}\\{save_name}.txt"
 names, means, errors = mean_lengths(path)
 lines = []
 
+lines.append("Density (kg/m^3): \n")
+lines.append("Surface tension (mNm): \n")
+lines.append("Viscosity (mPa s): \n")
+lines.append("Needle diameter (micrometers): \n")
+lines.append("\n")
+
 for name, mean, error in zip(names, means, errors):
     lines.append(f"{name}:\n")
-    lines.append(f"mean jet length: {mean}\n")
-    lines.append(f"standard deviation: {error}\n")
+    lines.append("flow rate (g/min): \n")
+    lines.append(f"mean jet length (mm): {mean}\n")
+    lines.append(f"standard deviation (mm): {error}\n")
     lines.append("\n")
 
 with open(save_path, "w") as f:
